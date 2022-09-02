ab29f268ca1213dc14d53e368009d98c4e322193
Jason Perez <jasonper@andrew.cmu.edu>
2022-09-02 13:00:44 -0400

    Fixed typo on position

--- fib.py
+++ fib.py
@@ -8,7 +8,7 @@ Negative numbers should return None
 def fibonacci(position):
   if(position == 0):
     return 0
-  if(postition == 1):
+  if(position == 1):
     return 1
   return fibonacci(position - 1) + fibonacci(position - 2)
 
